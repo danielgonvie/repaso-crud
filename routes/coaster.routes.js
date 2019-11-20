a4345c5c8dd9d67e08a96b04495d2c7c76a82da3
Daniel <daniel.gonvie@gmail.com>
2019-11-20 13:00:42 +0100

    Conforme

--- routes/coaster.routes.js
+++ routes/coaster.routes.js
@@ -1,7 +1,66 @@
 const express = require('express')
 const router = express.Router()
-
+const Coaster = require("../models/coaster.model.js");
+const Park = require("../models/park.model.js");
 // Aquí los endpoints
+/* router.get("/new", (req, res, next) => res.render("coasters/new-coaster")); */
 
+router.get("/", (req, res, next) => {
+    Coaster.find({}).populate("park")
+      .then(allCoaster =>
+        res.render("coasters/coasters-index", {coasters: allCoaster})
+      )
+      .catch(function() {
+        next();
+        throw new Error("There's an error.");
+      });
+  });
+
+
+
+
+router.get("/new", (req, res, next) => {
+    Park.find({})
+      .then(allParks =>
+        res.render("coasters/new-coaster", {parks: allParks})
+        
+      )
+      .catch(function() {
+        next();
+        throw new Error("There's an error.");
+      });
+  });
+router.post("/new", (req, res, next) => {
+    Coaster.create({
+        name: req.body.name,
+        description: req.body.description,
+        inversions:	req.body.inversions,
+        length:	req.body.length,
+        active:	true,
+        park: req.body.park,
+    })
+      .then(() => res.redirect("/new"))
+      .catch(function() {
+        next();
+        throw new Error("Como puedes ser tan desgraciado de no ser capaz de añadir algo!");
+      });
+    })
+
+
+      router.get("/:id", (req, res, next) => {
+        
+        Coaster.findById(req.params.id).populate("park")
+          .then(coaster => {
+                res.render("coasters/coaster-details", { coaster })
+
+        })
+          
+          .catch(function() {
+            next();
+            throw new Error("There's an error 2.0!");
+          });
+      });
+      
+    
 
 module.exports = router
--- routes/park.routes.js
+++ routes/park.routes.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const router = express.Router()
-
+const Park = require("../models/park.model.js");
 // Aquí los endpoints
+
+
+router.get("/new", (req, res, next) => res.render("parks/new-park"));
+router.post("/new", (req, res, next) => {
+    Park.create({
+      name: req.body.name,
+      description: req.body.description,
+      active: true,
+      
+    })
+      .then(() => res.redirect("/parks/new"))
+      .catch(function() {
+        next();
+        throw new Error("Como puedes ser tan desgraciado de no ser capaz de añadir algo!");
+      });
+    })
 
 module.exports = router
